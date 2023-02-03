6ad57cd4aae4cb3cfb7721adc6e3543ea3f80042
hanzhang <qingeneral@gmail.com>
2023-02-03 16:20:31 +0800

    modify publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,15 @@
-git branch -D gh-pages
-git checkout -b gh-pages
+git checkout main
+git add .
+git commit -m "add article ***"
+
+git checkout gh-pages
+git merge main
+
 book sm
 sed -i '' 's/%20/ /g' SUMMARY.md
+nvm use 10.24.0
 gitbook build
 cp -r _book/* .
 git add .
 git commit -m "publish book"
-git push -f --set-upstream github gh-pages
-git checkout main
+git push
